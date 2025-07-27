1bd0d3fe31a89cbbf820248ae17cf85127a1bad9
Ryan <git@hexa.mozmail.com>
2025-07-27 15:23:23 -0400

    format

--- mbta_mcp/_version.py
+++ mbta_mcp/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev1+gc9afdc7.d20250727"
-__version_tuple__ = version_tuple = (0, 1, "dev1", "gc9afdc7.d20250727")
+__version__ = version = "0.1.dev2+gf74147a"
+__version_tuple__ = version_tuple = (0, 1, "dev2", "gf74147a")
